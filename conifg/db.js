e833e07e3eb0bf522ae834b1c468d4ab5c839fa3
kishan <varmajaykishan63@gmail.com>
2024-12-19 19:11:22 +0530

    connect mongodb

--- conifg/db.js
+++ conifg/db.js
@@ -1,11 +1,29 @@
 // db.js
 import neo4j from "neo4j-driver";
 import dotenv from 'dotenv';
+import mongoose from "mongoose";
 dotenv.config();
 console.log("Neo4j URI:", process.env.NEO4J_URI);
+const DB_NAME="mandli2O"
+const connectDb = async ()=>{
+    try {
+        const connectionInstance = await mongoose.connect(process.env.MONGO_URI, {
+            dbName: DB_NAME, // Explicitly set the database name
+        })
+        console.log(`MongoDb Connected !!! at DB Host :: ${connectionInstance.connection.host}`)
+        
+
+    } catch (error) {
+        console.log("MONGODB connection Error :: ",error)
+        process.exit(1)
+    }
+}
+
+connectDb()
 const driver = neo4j.driver(
     process.env.NEO4J_URI,
     neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD)
 );
 
 export default driver;
+
